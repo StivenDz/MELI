fdbdb148bcd66864e9dd70022dd6e99fee770246
StivenDz <stivendiazh@gmail.com>
2022-08-20 15:49:39 -0500

    starting final react project

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+module.exports = {
+    entry: './src/index.js',
+    output:{
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    mode:'development',
+    resolve:{
+        extensions: ['.js', '.jsx'],
+        alias:{
+            '@components': path.join(__dirname, 'src/components/'),
+			'@containers': path.join(__dirname, 'src/containers/'),
+			'@pages': path.join(__dirname, 'src/pages/'),
+			'@routes': path.join(__dirname, 'src/routes/'),
+			'@styles': path.join(__dirname, 'src/styles/'),
+			'@icons': path.join(__dirname, 'src/assets/icons/'),
+			'@logos': path.join(__dirname, 'src/assets/logos/'),
+        }
+    },
+
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use:[
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(css)$/,
+                use:[
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.(scss)$/,
+                use:[
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.(png)$/,
+                type: 'asset'
+            },
+            {
+                test: /\.(svg)$/,
+                type: 'asset'
+            },
+            {
+                test: /\.(jpg)$/,
+                type: 'asset'
+            },
+            {
+                test: /\.(gif)$/,
+                type: 'asset'
+            },
+        ]
+    },
+
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    
+    devServer:{
+        historyApiFallback: true,
+    }
+}
